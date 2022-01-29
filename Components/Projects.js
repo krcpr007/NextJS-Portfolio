b9c89ba064eabce06eec5e14c111cf3f09f5a634
Rajan kumar <krcpr007@gmail.com>
2022-01-29 17:45:21 +0530

    Project pic integrates with next/image

--- Components/Projects.js
+++ Components/Projects.js
@@ -1,4 +1,9 @@
 import { FaGithubAlt } from 'react-icons/fa';
+import Image from 'next/image'
+import newsmainia from '../public/newsmania.jpg'
+import  github from '../public/github.jpg'
+import  marvelhub from '../public/marvelhub.jpg'
+import  bhartiTheme from '../public/theme.jpg'
 const Project=(props)=>{
     return(
         <>
@@ -12,7 +17,7 @@ const Project=(props)=>{
                     <div className="md:flex">
                         <div className=''>
                         <ul className='list-disc list-inside'>
-                            <li>This is a <span className='font-medium'>VS code dark theme</span> named Bharti theme made with help of <a href="https://www.npmjs.com/package/generator-code" target="_blank" rel="noreferrer" className='hover:underline text-emerald-300 font-medium'>Yo Code</a> - Extension and Customization Generator. <br /></li>
+                            <li>This is a <span className='font-medium text-emerald-300'>VS code dark theme</span> named Bharti theme made with help of <a href="https://www.npmjs.com/package/generator-code" target="_blank" rel="noreferrer" className='hover:underline text-emerald-300 font-medium'>Yo Code</a> - Extension and Customization Generator. <br /></li>
                             <li>It has medium contrast a or lite contrast theme. <br /></li>
                             <li>Dark navy blue have editor color <br /></li>
                             <li> You can download it from vs code extension marketplace and you can use it. <br /></li>
@@ -21,37 +26,37 @@ const Project=(props)=>{
                         </div>
                         <div className="">
                             <a href="https://raw.githubusercontent.com/krcpr007/BhartiTheme/master/image/bharti_theme2.jpg" target="_blank" rel="noreferrer" >
-                                <img src="https://raw.githubusercontent.com/krcpr007/BhartiTheme/master/image/bharti_theme2.jpg" className='shadow border-2 border-emerald-300 rounded-md w-full ' alt="" />
+                                <Image src={bhartiTheme} className='shadow-xl border-2 border-emerald-300 rounded-md w-full ' alt=""  />
                             </a>
                         </div>
                     </div>
                 </div>
                 <div className="">
                     <div className="text-gray-400">
-                    <a href="https://github.com/krcpr007/NEWSMAiNIA" rel="noreferrer" target="_blank" > <h2 className='text-xl my-2 font-semibold' > <span className='text-emerald-300' >02. </span>NEWS MAiNIA<FaGithubAlt className='inline text-emerald-300' />  </h2> </a>
-                    <div className='md:flex'>
+                        <a href="https://github.com/krcpr007/github-finder-ReactJS" rel="noreferrer" target="_blank" > <h2 className='text-xl my-2 font-semibold' > <span className='text-emerald-300' >02. </span>Github Finder<FaGithubAlt className='inline text-emerald-300' />  </h2> </a>
+                        <div className='md:flex'>
 
-                        <div>
-                            <ul className='list-disc list-inside'>
-                                <li>This is React web app where we can read lots of news.  </li>
-                                <li>Category-wise news (ex-science,business,sports)</li>
-                                <li>It made by using <a href="https://newsapi.org/" target="_blank" rel="noreferrer">NEWS24 API</a> and<a href="https://reactjs.org/" target="_blank" rel="noreferrer"> Reactjs </a> </li>
-                                <li>
-                                <span className='text-emerald-300 font-medium'>News API</span> is a simple, easy-to-use REST API that returns JSON search results for current and historic news articles published by over 80,000 worldwide sources.
-                                </li>
-                            </ul>
-                         </div>
+                            <div>
+                                <ul className='list-disc list-inside'>
+                                    <li>This is <span className="text-emerald-300 font-medium">React web app</span> where we can find github users </li>
+                                    <li>You can see also user profile pic and repositories </li>
+                                    <li>It made by using <a href="https://docs.github.com/en/rest" target="_blank" rel="noreferrer" className="text-emerald-300 font-medium">Github API</a> and<a href="https://reactjs.org/" target="_blank" rel="noreferrer" className="text-emerald-300 font-medium"> ReactJS </a> </li>
+                                    <li>
+                                        <a href="https://docs.github.com/en/rest" target="_blank" rel="noreferrer"> <span className='text-emerald-300 font-medium'>Github API</span> </a> is a simple, easy-to-use REST API that returns JSON search results for current and historic news articles published by over 80,000 worldwide sources.
+                                    </li>
+                                </ul>
+                            </div>
                             <div className="">
-                                <a href="https://raw.githubusercontent.com/krcpr007/NEWSMAiNIA/main/public/images/Readme.jpg">
-                                    <img src="https://raw.githubusercontent.com/krcpr007/NEWSMAiNIA/main/public/images/Readme.jpg" className='shadow border-2 border-emerald-300 rounded-md w-full ' alt="projectimg" />
+                                <a href="https://github.com/krcpr007/github-finder-ReactJS/blob/main/public/image1.jpg">
+                                    <Image src={github} className='shadow-xl border-2 border-emerald-300 rounded-md w-full ' alt="projectimg" />
                                 </a>
-                    </div>
-                    </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-        <div className="project1">
-                <div className="project1-abt">
-                    <a href="https://github.com/krcpr007/MarvelHub" rel="noreferrer" target="_blank" style={{ color: "#CCD6F6" }} > <h2 className='text-xl my-2 font-semibold' > <span className='text-emerald-300' >03. </span>Marvel HuB<FaGithubAlt className='text-emerald-300 inline' />  </h2> </a>
+        <div className="">
+                <div className="text-gray-400">
+                    <a href="https://github.com/krcpr007/MarvelHub" rel="noreferrer" target="_blank" > <h2 className='text-xl my-2 font-semibold' > <span className='text-emerald-300' >02. </span>Marvel HuB<FaGithubAlt className='text-emerald-300 inline' />  </h2> </a>
                     <div className='md:flex'>
                     <div>
                         <ul className='list-disc list-inside'> 
@@ -61,12 +66,36 @@ const Project=(props)=>{
                         </ul>
                     </div>
                         <div className="project-img">
-                            <img src="https://source.unsplash.com/1600x900/daily?marvel" className='shadow border-2 border-emerald-300 rounded-md w-full'  alt="project1img" />
+                            <Image src={marvelhub} className='shadow border-2 border-emerald-300 rounded-md w-full'  alt="project1img" />
                             {/* <img src="https://raw.githubusercontent.com/krcpr007/MarvelHub/blob/main/public/image2.jpg" alt="" /> */}
                         </div>
                     </div>
                 </div>
          </div>
+
+                <div className="">
+                    <div className="text-gray-400">
+                        <a href="https://github.com/krcpr007/NEWSMAiNIA" rel="noreferrer" target="_blank" > <h2 className='text-xl my-2 font-semibold' > <span className='text-emerald-300' >03. </span>NEWS MAiNIA<FaGithubAlt className='inline text-emerald-300' />  </h2> </a>
+                        <div className='md:flex'>
+
+                            <div>
+                                <ul className='list-disc list-inside'>
+                                    <li>This is React web app where we can read lots of news.  </li>
+                                    <li>Category-wise news (ex-science,business,sports)</li>
+                                    <li>It made by using <a href="https://newsapi.org/" target="_blank" rel="noreferrer">NEWS24 API</a> and<a href="https://reactjs.org/" target="_blank" rel="noreferrer"> Reactjs </a> </li>
+                                    <li>
+                                        <span className='text-emerald-300 font-medium'>News API</span> is a simple, easy-to-use REST API that returns JSON search results for current and historic news articles published by over 80,000 worldwide sources.
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="">
+                                <a href="https://raw.githubusercontent.com/krcpr007/NEWSMAiNIA/main/public/images/Readme.jpg">
+                                    <Image src={newsmainia} className='shadow border-2 border-emerald-300 rounded-md w-full ' alt="projectimg" />
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
             </div>
         </div>
