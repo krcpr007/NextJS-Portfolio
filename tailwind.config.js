53633d31a43455620c4fec4ee53754185f33f02f
Rajan kumar <krcpr007@gmail.com>
2022-02-03 13:53:18 +0530

    trying to deploy on vercell

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+    // content: ["./pages/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}", "./Layout/**/*.{js,jsx,ts,tsx}"],
+
+  purge: {
+    content: ["./src/pages/**/*.{js,jsx,ts,tsx}", "./src/components/**/*.{js,jsx,ts,tsx}"],
+    // content: ["./pages/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}", "./Layout/**/*.{js,jsx,ts,tsx}"],
+    // These options are passed through directly to PurgeCSS
+  },
   theme: {
     extend: {
      
@@ -23,3 +29,13 @@ module.exports = {
     ],
   ],
 }
+// module.exports = {
+//   content: [
+//     "./pages/**/*.{js,ts,jsx,tsx}",
+//     "./components/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
