e67d1b3dae1504c60c70dd43836e96bf18b00740
Rajan kumar <krcpr007@gmail.com>
2022-03-12 13:03:43 +0530

    config chang

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,50 @@
+// module.exports = {
+//   mode:"jit",
+//   content: [
+//     './pages/**/*.{js,ts,jsx,tsx}',
+//     './components/**/*.{js,ts,jsx,tsx}',
+//     './Layout/**/*.{js,ts,jsx,tsx}',
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
 module.exports = {
-  mode:"jit",
-  purge: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './Layout/**/*.{js,ts,jsx,tsx}',
-  ],
+  mode: "jit",
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "2xl": "1500px",
+      },
+      keyframes: {
+        pulse: {
+          "0%, 100%": {
+            opacity: 1,
+          },
+          "50%": {
+            opacity: 0.5,
+          },
+        },
+      },
+      animation: {
+        pulse: "pulse 4s cubic-bezier(0.4, 0, 0.6, 1) infinite",
+      },
+    },
+    fontFamily: {
+      body: ["Andada Pro", "serif"],
+      sans: ["ui-sans-serif", "system-ui"],
+    },
   },
-  plugins: [],
-}
+  variants: {
+    extend: {},
+    scrollbar: ["rounded"],
+  },
+  plugins: [
+    // require("tailwind-scrollbar-hide"),
+    // require("@tailwindcss/forms"),
+    // require("tailwind-scrollbar"),
+  ],
+};
