3055eef45cf1f510a3b063b97ce0f76eb9ca0eb4
Rajan kumar <krcpr007@gmail.com>
2022-05-24 11:59:09 +0530

    navbar srcipt added

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
 import Name from '../Components/Name'
 import About from '../Components/About'
 import Projects from '../Components/Projects'
+import Script from 'next/script'
 export default function Home() {
   return (
     <>
-    <Name/>
-    <About/>
-    <Projects/>
+    {/* Navbar imported from layout */}
+    <Script src='/script.js'></Script>
+      <Name/>
+      <About/>
+      <Projects/>
+    {/* Footer is also imported from layout */}
     </>
   )
 }
